af8a0cbaa391850d12a5d2e1d8b14b323c8dd918
DilaAmaliaDae <amaliadila95@gmail.com>
2017-05-17 20:07:27 +0800

    Create function.js

---
+++ function.js
@@ -0,0 +1,474 @@
+function fungsiPilihan(ConvertorNumber)
+{
+	switch (ConvertorNumber.value)
+	{
+		case "cf":
+		{
+			document.getElementById("idShow1").style.display = "block";
+			document.getElementById("idShow2").style.display = "none";
+			document.getElementById("idShow3").style.display = "none";
+			document.getElementById("idShow4").style.display = "none";
+			document.getElementById("idShow5").style.display = "none";
+			document.getElementById("idShow6").style.display = "none";
+			document.getElementById("idShow7").style.display = "none";
+			document.getElementById("idShow8").style.display = "none";
+			document.getElementById("idShow9").style.display = "none";
+			document.getElementById("idShow10").style.display = "none";
+			document.getElementById("idShow11").style.display = "none";
+			document.getElementById("idShow12").style.display = "none";
+			break;
+		}
+		case "cr":
+		{
+			document.getElementById("idShow1").style.display = "none";
+			document.getElementById("idShow2").style.display = "block";
+			document.getElementById("idShow3").style.display = "none";
+			document.getElementById("idShow4").style.display = "none";
+			document.getElementById("idShow5").style.display = "none";
+			document.getElementById("idShow6").style.display = "none";
+			document.getElementById("idShow7").style.display = "none";
+			document.getElementById("idShow8").style.display = "none";
+			document.getElementById("idShow9").style.display = "none";
+			document.getElementById("idShow10").style.display = "none";
+			document.getElementById("idShow11").style.display = "none";
+			document.getElementById("idShow12").style.display = "none";
+			break;
+		}
+		case "ck":
+		{
+			document.getElementById("idShow1").style.display = "none";
+			document.getElementById("idShow2").style.display = "none";
+			document.getElementById("idShow3").style.display = "block";
+			document.getElementById("idShow4").style.display = "none";
+			document.getElementById("idShow5").style.display = "none";
+			document.getElementById("idShow6").style.display = "none";
+			document.getElementById("idShow7").style.display = "none";
+			document.getElementById("idShow8").style.display = "none";
+			document.getElementById("idShow9").style.display = "none";
+			document.getElementById("idShow10").style.display = "none";
+			document.getElementById("idShow11").style.display = "none";
+			document.getElementById("idShow12").style.display = "none";
+			break;
+		}
+		case "fc":
+		{
+			document.getElementById("idShow1").style.display = "none";
+			document.getElementById("idShow2").style.display = "none";
+			document.getElementById("idShow3").style.display = "none";
+			document.getElementById("idShow4").style.display = "block";
+			document.getElementById("idShow5").style.display = "none";
+			document.getElementById("idShow6").style.display = "none";
+			document.getElementById("idShow7").style.display = "none";
+			document.getElementById("idShow8").style.display = "none";
+			document.getElementById("idShow9").style.display = "none";
+			document.getElementById("idShow10").style.display = "none";
+			document.getElementById("idShow11").style.display = "none";
+			document.getElementById("idShow12").style.display = "none";
+			break;
+		}
+		case "fr":
+		{
+			document.getElementById("idShow1").style.display = "none";
+			document.getElementById("idShow2").style.display = "none";
+			document.getElementById("idShow3").style.display = "none";
+			document.getElementById("idShow4").style.display = "none";
+			document.getElementById("idShow5").style.display = "block";
+			document.getElementById("idShow6").style.display = "none";
+			document.getElementById("idShow7").style.display = "none";
+			document.getElementById("idShow8").style.display = "none";
+			document.getElementById("idShow9").style.display = "none";
+			document.getElementById("idShow10").style.display = "none";
+			document.getElementById("idShow11").style.display = "none";
+			document.getElementById("idShow12").style.display = "none";
+			break;
+		}
+		case "fk":
+		{
+
+			document.getElementById("idShow1").style.display = "none";
+			document.getElementById("idShow2").style.display = "none";
+			document.getElementById("idShow3").style.display = "none";
+			document.getElementById("idShow4").style.display = "none";
+			document.getElementById("idShow5").style.display = "none";
+			document.getElementById("idShow6").style.display = "block";
+			document.getElementById("idShow7").style.display = "none";
+			document.getElementById("idShow8").style.display = "none";
+			document.getElementById("idShow9").style.display = "none";
+			document.getElementById("idShow10").style.display = "none";
+			document.getElementById("idShow11").style.display = "none";
+			document.getElementById("idShow12").style.display = "none";
+			break;
+		}
+		case "rc":
+		{
+			document.getElementById("idShow1").style.display = "none";
+			document.getElementById("idShow2").style.display = "none";
+			document.getElementById("idShow3").style.display = "none";
+			document.getElementById("idShow4").style.display = "none";
+			document.getElementById("idShow5").style.display = "none";
+			document.getElementById("idShow6").style.display = "none";
+			document.getElementById("idShow7").style.display = "block";
+			document.getElementById("idShow8").style.display = "none";
+			document.getElementById("idShow9").style.display = "none";
+			document.getElementById("idShow10").style.display = "none";
+			document.getElementById("idShow11").style.display = "none";
+			document.getElementById("idShow12").style.display = "none";
+			break;
+		}
+		case "rf":
+		{
+			document.getElementById("idShow1").style.display = "none";
+			document.getElementById("idShow2").style.display = "none";
+			document.getElementById("idShow3").style.display = "none";
+			document.getElementById("idShow4").style.display = "none";
+			document.getElementById("idShow5").style.display = "none";
+			document.getElementById("idShow6").style.display = "none";
+			document.getElementById("idShow7").style.display = "none";
+			document.getElementById("idShow8").style.display = "block";
+			document.getElementById("idShow9").style.display = "none";
+			document.getElementById("idShow10").style.display = "none";
+			document.getElementById("idShow11").style.display = "none";
+			document.getElementById("idShow12").style.display = "none";
+			break;
+		}
+		case "rk":
+		{
+			document.getElementById("idShow1").style.display = "none";
+			document.getElementById("idShow2").style.display = "none";
+			document.getElementById("idShow3").style.display = "none";
+			document.getElementById("idShow4").style.display = "none";
+			document.getElementById("idShow5").style.display = "none";
+			document.getElementById("idShow6").style.display = "none";
+			document.getElementById("idShow7").style.display = "none";
+			document.getElementById("idShow8").style.display = "none";
+			document.getElementById("idShow9").style.display = "block";
+			document.getElementById("idShow10").style.display = "none";
+			document.getElementById("idShow11").style.display = "none";
+			document.getElementById("idShow12").style.display = "none";
+			break;
+		}
+		case "kc":
+		{
+			document.getElementById("idShow1").style.display = "none";
+			document.getElementById("idShow2").style.display = "none";
+			document.getElementById("idShow3").style.display = "none";
+			document.getElementById("idShow4").style.display = "none";
+			document.getElementById("idShow5").style.display = "none";
+			document.getElementById("idShow6").style.display = "none";
+			document.getElementById("idShow7").style.display = "none";
+			document.getElementById("idShow8").style.display = "none";
+			document.getElementById("idShow9").style.display = "none";
+			document.getElementById("idShow10").style.display = "block";
+			document.getElementById("idShow11").style.display = "none";
+			document.getElementById("idShow12").style.display = "none";
+			break;
+		}
+		case "kf":
+		{
+			document.getElementById("idShow1").style.display = "none";
+			document.getElementById("idShow2").style.display = "none";
+			document.getElementById("idShow3").style.display = "none";
+			document.getElementById("idShow4").style.display = "none";
+			document.getElementById("idShow5").style.display = "none";
+			document.getElementById("idShow6").style.display = "none";
+			document.getElementById("idShow7").style.display = "none";
+			document.getElementById("idShow8").style.display = "none";
+			document.getElementById("idShow9").style.display = "none";
+			document.getElementById("idShow10").style.display = "none";
+			document.getElementById("idShow11").style.display = "block";
+			document.getElementById("idShow12").style.display = "none";
+			break;
+		}
+		case "kr":
+		{
+			document.getElementById("idShow1").style.display = "none";
+			document.getElementById("idShow2").style.display = "none";
+			document.getElementById("idShow3").style.display = "none";
+			document.getElementById("idShow4").style.display = "none";
+			document.getElementById("idShow5").style.display = "none";
+			document.getElementById("idShow6").style.display = "none";
+			document.getElementById("idShow7").style.display = "none";
+			document.getElementById("idShow8").style.display = "none";
+			document.getElementById("idShow9").style.display = "none";
+			document.getElementById("idShow10").style.display = "none";
+			document.getElementById("idShow11").style.display = "none";
+			document.getElementById("idShow12").style.display = "block";
+			break;
+		}
+		case "ru":
+		{
+			document.getElementById("idShow14").style.display = "none";
+			document.getElementById("idShow15").style.display = "none";
+			document.getElementById("idShow16").style.display = "none";
+			document.getElementById("idShow13").style.display = "block";
+			document.getElementById("idShow17").style.display = "none";
+			document.getElementById("idShow18").style.display = "none";
+			document.getElementById("idShow19").style.display = "none";
+			document.getElementById("idShow20").style.display = "none";
+			break;
+		}
+		case "ry":
+		{
+			document.getElementById("idShow13").style.display = "none";
+			document.getElementById("idShow14").style.display = "block";			
+			document.getElementById("idShow15").style.display = "none";
+			document.getElementById("idShow16").style.display = "none";
+			document.getElementById("idShow17").style.display = "none";
+			document.getElementById("idShow18").style.display = "none";
+			document.getElementById("idShow19").style.display = "none";
+			document.getElementById("idShow20").style.display = "none";
+			break;
+		}
+		case "re":
+		{
+			document.getElementById("idShow13").style.display = "none";
+			document.getElementById("idShow14").style.display = "none";			
+			document.getElementById("idShow15").style.display = "block";
+			document.getElementById("idShow16").style.display = "none";
+			document.getElementById("idShow17").style.display = "none";
+			document.getElementById("idShow18").style.display = "none";
+			document.getElementById("idShow19").style.display = "none";
+			document.getElementById("idShow20").style.display = "none";
+			break;
+		}
+		case "rr":
+		{
+			document.getElementById("idShow13").style.display = "none";
+			document.getElementById("idShow14").style.display = "none";
+			document.getElementById("idShow15").style.display = "none";
+			document.getElementById("idShow16").style.display = "block";
+			document.getElementById("idShow17").style.display = "none";
+			document.getElementById("idShow18").style.display = "none";
+			document.getElementById("idShow19").style.display = "none";
+			document.getElementById("idShow20").style.display = "none";
+			break;
+		}
+		case "ur":
+		{
+			document.getElementById("idShow13").style.display = "none";
+			document.getElementById("idShow14").style.display = "none";
+			document.getElementById("idShow15").style.display = "none";
+			document.getElementById("idShow16").style.display = "none";
+			document.getElementById("idShow17").style.display = "block";
+			document.getElementById("idShow18").style.display = "none";
+			document.getElementById("idShow19").style.display = "none";
+			document.getElementById("idShow20").style.display = "none";
+			break;
+		}
+		case "uy":
+		{
+			document.getElementById("idShow13").style.display = "none";
+			document.getElementById("idShow14").style.display = "none";
+			document.getElementById("idShow15").style.display = "none";
+			document.getElementById("idShow16").style.display = "none";
+			document.getElementById("idShow17").style.display = "none";
+			document.getElementById("idShow18").style.display = "block";
+			document.getElementById("idShow19").style.display = "none";
+			document.getElementById("idShow20").style.display = "none";
+			break;
+		}
+		case "ue":
+		{
+			document.getElementById("idShow13").style.display = "none";
+			document.getElementById("idShow14").style.display = "none";
+			document.getElementById("idShow15").style.display = "none";
+			document.getElementById("idShow16").style.display = "none";
+			document.getElementById("idShow17").style.display = "none";
+			document.getElementById("idShow18").style.display = "none";
+			document.getElementById("idShow19").style.display = "block";
+			document.getElementById("idShow20").style.display = "none";
+			break;
+		}
+		case "urr":
+		{
+			document.getElementById("idShow13").style.display = "none";
+			document.getElementById("idShow14").style.display = "none";
+			document.getElementById("idShow15").style.display = "none";
+			document.getElementById("idShow16").style.display = "none";
+			document.getElementById("idShow17").style.display = "none";
+			document.getElementById("idShow18").style.display = "none";
+			document.getElementById("idShow19").style.display = "none";
+			document.getElementById("idShow20").style.display = "block";
+			break;
+		}
+		case "gk":
+		{
+			document.getElementById("idShow22").style.display = "none";
+			document.getElementById("idShow23").style.display = "none";
+			document.getElementById("idShow24").style.display = "none";
+			document.getElementById("idShow21").style.display = "block";
+			document.getElementById("idShow25").style.display = "none";
+
+			break;
+		}
+		case "go":
+		{
+			document.getElementById("idShow21").style.display = "none";
+			document.getElementById("idShow23").style.display = "none";
+			document.getElementById("idShow24").style.display = "none";
+			document.getElementById("idShow22").style.display = "block";
+			document.getElementById("idShow25").style.display = "none";
+			break;
+		}
+		case "gd":
+		{
+			document.getElementById("idShow21").style.display = "none";
+			document.getElementById("idShow22").style.display = "none";
+			document.getElementById("idShow24").style.display = "none";
+			document.getElementById("idShow23").style.display = "block";
+			document.getElementById("idShow25").style.display = "none";
+			break;
+		}
+		case "kt":
+		{
+			document.getElementById("idShow21").style.display = "none";
+			document.getElementById("idShow22").style.display = "none";
+			document.getElementById("idShow23").style.display = "none";
+			document.getElementById("idShow24").style.display = "block";
+			document.getElementById("idShow25").style.display = "none";
+			break;
+		}
+		case "kg":
+		{
+			document.getElementById("idShow21").style.display = "none";
+			document.getElementById("idShow22").style.display = "none";
+			document.getElementById("idShow23").style.display = "none";
+			document.getElementById("idShow24").style.display = "none";
+			document.getElementById("idShow25").style.display = "block";
+			break;
+		}
+
+
+
+	}
+}
+
+
+function temperatureConverter1 (valNum) 
+{
+	valNum = parseFloat(valNum);
+	document.getElementById('outputFahrenheit1').innerHTML = (1.8 *valNum) + 32;
+}
+
+function temperatureConverter2 (valNum) 
+{
+	valNum = parseFloat(valNum);
+	document.getElementById('outputReamur1').innerHTML = valNum*0.8;
+}
+
+function temperatureConverter3 (valNum) 
+{
+	valNum = parseFloat(valNum);
+	document.getElementById('outputKelvin1').innerHTML = (1*valNum) + 273.15;
+}
+
+function temperatureConverter4 (valNum) 
+{
+	valNum = parseFloat(valNum);
+	document.getElementById('outputCelcius1').innerHTML = (valNum-32)/1.8;
+}
+function temperatureConverter5 (valNum) 
+{
+	valNum = parseFloat(valNum);
+	document.getElementById('outputReamur2').innerHTML = (valNum-32)/2.25;
+}
+function temperatureConverter6 (valNum) 
+{
+	valNum = parseFloat(valNum);
+	document.getElementById('outputKelvin2').innerHTML = (valNum-459.67)/1.8;
+}
+function temperatureConverter7 (valNum) 
+{
+	valNum = parseFloat(valNum);
+	document.getElementById('outputCelcius2').innerHTML = (1*valNum)/0.8;
+}
+function temperatureConverter8 (valNum) 
+{
+	valNum = parseFloat(valNum);
+	document.getElementById('outputFahrenheit2').innerHTML = (2.25*valNum)+32;
+}
+function temperatureConverter9 (valNum) 
+{
+	valNum = parseFloat(valNum);
+	document.getElementById('outputKelvin3').innerHTML = (1.25*valNum)+273.15;
+}
+function temperatureConverter10 (valNum) 
+{
+	valNum = parseFloat(valNum);
+	document.getElementById('outputCelcius3').innerHTML = (1*valNum)-273.15;
+}
+function temperatureConverter11 (valNum) 
+{
+	valNum = parseFloat(valNum);
+	document.getElementById('outputFahrenheit3').innerHTML = (valNum*1.8)-459.67;
+}
+function temperatureConverter12 (valNum) 
+{
+	valNum = parseFloat(valNum);
+	document.getElementById('outputReamur3').innerHTML = (valNum-273.15)*0.8;
+}
+function temperatureConverter13 (valNum) 
+{
+	valNum = parseFloat(valNum);
+	document.getElementById('outputUSD').innerHTML = (1*valNum)/13286.16;
+}
+function temperatureConverter14 (valNum) 
+{
+	valNum = parseFloat(valNum);
+	document.getElementById('outputYen1').innerHTML = (1*valNum)*0.085;
+}
+function temperatureConverter15 (valNum) 
+{
+	valNum = parseFloat(valNum);
+	document.getElementById('outputEuro1').innerHTML = (1*valNum)*0.0001;
+}
+function temperatureConverter16 (valNum) 
+{
+	valNum = parseFloat(valNum);
+	document.getElementById('outputRinggit1').innerHTML = (1*valNum)*0.0003;
+}
+function temperatureConverter17 (valNum) 
+{
+	valNum = parseFloat(valNum);
+	document.getElementById('outputRupiah1').innerHTML = (1*valNum)/13286.16;
+}
+function temperatureConverter18 (valNum) 
+{
+	valNum = parseFloat(valNum);
+	document.getElementById('outputYen2').innerHTML = (1*valNum)*112.74;
+}
+function temperatureConverter19 (valNum) 
+{
+	valNum = parseFloat(valNum);
+	document.getElementById('outputEuro2').innerHTML = (1*valNum)*0.91;
+}
+function temperatureConverter20 (valNum) 
+{
+	valNum = parseFloat(valNum);
+	document.getElementById('outputRinggit2').innerHTML = (1*valNum)/4.43;
+}
+function temperatureConverter21 (valNum) 
+{
+	valNum = parseFloat(valNum);
+	document.getElementById('outputKilogram1').innerHTML = (1*valNum)/1000;
+}
+function temperatureConverter22 (valNum) 
+{
+	valNum = parseFloat(valNum);
+	document.getElementById('outputOns1').innerHTML = (1*valNum)*100;
+}
+function temperatureConverter23 (valNum) 
+{
+	valNum = parseFloat(valNum);
+	document.getElementById('outputDekagram1').innerHTML = (1*valNum)*0.1;
+}
+function temperatureConverter24 (valNum) 
+{
+	valNum = parseFloat(valNum);
+	document.getElementById('outputTon1').innerHTML = (1*valNum)/1000;
+}
+function temperatureConverter25 (valNum) 
+{
+	valNum = parseFloat(valNum);
+	document.getElementById('outputGram1').innerHTML = (1*valNum)*1000;
+}
